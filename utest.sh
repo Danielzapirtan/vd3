504529c6fc657ff3735c7cae443b0a9293fb4457
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-07-23 20:34:30 +0300

    Thu Jul 23 20:34:30 EEST 2026

--- utest.sh
+++ utest.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env bash
 
-VER=3.10
+VER=""
 
 if test -z $VIRTUAL_ENV; then
 	test -d venv || python$VER -m venv venv
 	source venv/bin/activate
 	export VIRTUAL_ENV
 fi
-apt install ffmpeg
+sudo apt install ffmpeg
 pip install -r requirements.txt
 python$VER app.py
 
